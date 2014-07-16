1c2d7a9384e8c6fbf2e2a3b15270c3c3ec8fb6db
Simon Whitehead <chemnova@gmail.com>
2014-07-16 23:14:04 +1000

    Initial commit

---
+++ num-id.go
@@ -0,0 +1,47 @@
+package numid
+
+import (
+	"bytes"
+	"math"
+	"strings"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func Encode(n int64) string {
+	result := bytes.NewBuffer(nil)
+	f := float64(n)
+	al := float64(len(alphabet))
+	for i := math.Floor(math.Log(f / math.Log(al))); i >= 0; i-- {
+		idx := int(math.Mod(math.Floor(f/bpow(al, i)), al))
+		result.WriteString(alphabet[idx : idx+1])
+	}
+
+	return reverseString(result.String())
+}
+
+func Decode(id string) int64 {
+	str := reverseString(id)
+	result := int64(0)
+	end := len(str) - 1
+	al := float64(len(alphabet))
+	for i := 0; i <= end; i++ {
+		result += int64(float64(strings.Index(alphabet, str[i:i+1])) * bpow(al, float64(end-i)))
+	}
+
+	return result
+}
+
+func reverseString(s string) string {
+	n := len(s)
+	runes := make([]rune, n)
+	for _, rune := range s {
+		n--
+		runes[n] = rune
+	}
+	return string(runes[n:])
+}
+
+func bpow(a float64, b float64) float64 {
+	return math.Floor(math.Pow(a, b))
+}
---
+++ num-id_test.go
@@ -0,0 +1,65 @@
+package numid
+
+import (
+	"testing"
+)
+
+func Test100EqualsCbaa(t *testing.T) {
+	input := int64(100)
+	expected := "Cbaa"
+	actual := Encode(input)
+
+	assertEqual(t, expected, actual)
+}
+
+func TestCbaaEquals100(t *testing.T) {
+	input := "Cbaa"
+	expected := int64(100)
+	actual := Decode(input)
+
+	assertEqual(t, expected, actual)
+}
+
+func TestEncodeDecode(t *testing.T) {
+	input := int64(1100)
+	expected := int64(1100)
+	actual := Decode(Encode(input))
+
+	assertEqual(t, expected, actual)
+}
+
+func TestFailEncodeDecode(t *testing.T) {
+	input := int64(1001)
+	expected := int64(101)
+	actual := Decode(Encode(input))
+
+	assertNotEqual(t, expected, actual)
+}
+
+func TestReverseBasicString(t *testing.T) {
+	input := "Hello world!"
+	expected := "!dlrow olleH"
+	actual := reverseString(input)
+
+	assertEqual(t, expected, actual)
+}
+
+func TestPalindrome(t *testing.T) {
+	input := "kayak"
+	expected := "kayak"
+	actual := reverseString(input)
+
+	assertEqual(t, expected, actual)
+}
+
+func assertNotEqual(t *testing.T, expected interface{}, actual interface{}) {
+	if expected == actual {
+		t.Errorf("Expected: %v, Actual: %v", expected, actual)
+	}
+}
+
+func assertEqual(t *testing.T, expected interface{}, actual interface{}) {
+	if expected != actual {
+		t.Errorf("Expected: %v, Actual: %v", expected, actual)
+	}
+}
